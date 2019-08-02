7ffa4b807cd692a9471312fc58387f7448840591
Kévin SEGUINEAU <kevinseguineau+git@gmail.com>
2019-08-02 11:14:12 +0200

    correction mdp en clair

--- src/main/java/dev/services/LoginService.java
+++ src/main/java/dev/services/LoginService.java
@@ -1,24 +1,29 @@
 package dev.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import dev.domains.User;
 import dev.exceptions.AppException;
 import dev.utils.DbUtils;
 
-import java.util.List;
-import java.util.Optional;
-
-
 public class LoginService {
 
-    public Optional<User> connect(String login, String password) {
-        List<User> results = new DbUtils().executeSelect(String.format("select * from user where login='%s' and password='%s'", login, password), resultSet -> new DbUtils().resultSetToUser(resultSet));
+	public Optional<User> connect(String login, String password) {
+		List<User> results = new DbUtils().executeSelect(String.format("select * from user where login='%s'", login,
+				password), resultSet -> new DbUtils().resultSetToUser(resultSet));
 
-        if(results.size() > 1) {
-            throw new AppException("at least 2 users with same login");
-        }
+		if (results.size() > 1) {
+			throw new AppException("at least 2 users with same login");
+		}
 
-        return results.stream().findAny();
-    }
+		return results.stream().findAny();
+	}
 
+	public boolean verifyPassword(String pClair, String pHash) {
+		return BCrypt.verifyer().verify(pClair.toCharArray(),
+				pHash).verified;
+	}
 
 }
---
+++ src/main/java/dev/utils/main.java
@@ -0,0 +1,20 @@
+package dev.utils;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
+public class main {
+
+	public main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		String password = "admin";
+		System.out.println(BCrypt.withDefaults().hashToString(12, password.toCharArray()));
+
+		BCrypt.Result result = BCrypt.verifyer().verify(password.toCharArray(),
+				"$2a$12$QFYX2f/bhL9MQ86lCOyAkuy9dZyiGjQs/FHek0YzGkX71PJOwQxXm");
+		System.out.println(result.verified);
+	}
+
+}
